c43e83051c65bb40a07b00c67da69d492c610fba
mcdave umoh <108699710+McdaveUmoh@users.noreply.github.com>
2022-07-05 05:43:53 +0000

    attempted challenge 1 & 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,11 +13,18 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
 
+
+function sumOfNumbers(arrayOfNumbers) {
+    let sum = 0
+    for ( x in arrayOfNumbers){
+      sum = sum + arrayOfNumbers[x]
+      
+    }
+    return sum;
+}
+
+assignment.sumOfNumbers = sumOfNumbers;
 /**
  * Challenge - 2
  * 
@@ -27,10 +34,19 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    evenNumbersArray = []
+    for (x in arrayOfNumbers){
+      if(arrayOfNumbers[x] % 2 == 0){
+        evenNumbersArray.push(arrayOfNumbers[x])
+      }
+    }
+  return evenNumbersArray.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
